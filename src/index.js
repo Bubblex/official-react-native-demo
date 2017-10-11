e76938ded01208909f085563881b96694894d3e0
江肖 <jiangxiao@1doo.com.cn>
2017-10-11 17:29:35 +0800

    提取 Welcome 组件

--- src/index.js
+++ src/index.js
@@ -1,55 +1,9 @@
-import React, { Component } from 'react';
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation'
-
-class Welcome extends Component {
-    static navigationOptions = {
-        title: 'Welcome'
-    };
-    render() {
-        return (
-        <View style={styles.container}>
-            <Text style={styles.welcome}>
-                Welcome to React Native!
-            </Text>
-            <Text style={styles.instructions}>
-                To get started, edit index.ios.js
-            </Text>
-            <Text style={styles.instructions}>
-                Press Cmd+R to reload,{'\n'}
-                Cmd+D or shake for dev menu
-            </Text>
-        </View>
-        );
-    }
-}
+import Welcome from './screen/home'
 
 export const MyApp = StackNavigator({
     Home: { screen: Welcome }
 })
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
 
 AppRegistry.registerComponent('MyApp', () => MyApp);
