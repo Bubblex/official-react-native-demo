235080061200854b0ac6acf6ad2070e0137bc8fa
江肖 <jiangxiao@1doo.com.cn>
2017-10-12 11:46:29 +0800

    使用 react-native-dva-starter-master 搭建

--- ios/DvaStarterTests/DvaStarterTests.m
+++ ios/DvaStarterTests/DvaStarterTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface MyAppTests : XCTestCase
+@interface DvaStarterTests : XCTestCase
 
 @end
 
-@implementation MyAppTests
+@implementation DvaStarterTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
